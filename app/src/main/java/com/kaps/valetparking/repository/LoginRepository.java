56addda84df00ccfd53cfbbc29d8b173101fbfb9
Fr1x3 <48664880+Fr1x3@users.noreply.github.com>
2019-12-11 12:07:24 +0300

    Update LoginRepository.java
    send data to login

--- app/src/main/java/com/kaps/valetparking/repository/LoginRepository.java
+++ app/src/main/java/com/kaps/valetparking/repository/LoginRepository.java
@@ -1,11 +1,35 @@
 package com.kaps.valetparking.repository;
 
 import com.kaps.valetparking.network.LoginService;
+import com.kaps.valetparking.network.ServiceBuilder;
+
+import okhttp3.Response;
+import retrofit2.Call;
+import retrofit2.Callback;
 
 public class LoginRepository {
 
     private LoginService mLoginService;
 
+    public LoginRepository(){
+        mLoginService = ServiceBuilder.createService(LoginService.class);
+    }
 
 
+    public void login(String email, String password){
+
+        mLoginService.login(email, password).enqueue(new Callback<Response>() {
+            @Override
+            public void onResponse(Call<Response> call, retrofit2.Response<Response> response) {
+                if( response.isSuccessful()){
+                    // confirm that the login is successfull
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Response> call, Throwable t) {
+
+            }
+        });
+    }
 }
