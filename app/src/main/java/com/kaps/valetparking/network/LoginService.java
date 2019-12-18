8266051c2f2c53694e31e4cf35fc259792fbbda2
Fr1x3 <48664880+Fr1x3@users.noreply.github.com>
2019-12-18 10:33:42 +0300

    has login and logout

--- app/src/main/java/com/kaps/valetparking/network/LoginService.java
+++ app/src/main/java/com/kaps/valetparking/network/LoginService.java
@@ -1,14 +1,21 @@
 package com.kaps.valetparking.network;
 
-import okhttp3.Response;
+import okhttp3.ResponseBody;
+
+
 import retrofit2.Call;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
-import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.PUT;
 
 public interface LoginService {
 
     @FormUrlEncoded
-    @GET("login")
-    Call<Response> login(@Field("email") String email, @Field("password") String password);
+    @POST("login")
+    Call<Integer> login(@Field("email") String email, @Field("password") String password);
+
+    @FormUrlEncoded
+    @PUT("login")
+    Call<Integer> logout(@Field("email") String email);
 }
